0c247f8e932c4fad6d38349717db9492ff209438
Francesco Serrani <FRANCESCO.SERRANI@local.ittcesena.it>
2022-10-08 08:38:15 +0200

    versione base: calcolo media

---
+++ git esempioVero/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace git_esempioVero
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMedia_Click(object sender, RoutedEventArgs e)
+        {
+            double[] v = new double[10];
+            Random rnd = new Random();
+
+            for (int i = 0; i < 10; i++)
+                v[i] = rnd.Next(0, 100);
+
+            double somma = 0;
+
+            for (int y = 0; y < 10; y++)
+                somma += v[y];
+            lblMedia.Content = somma / 10;
+        }
+    }
+}
